c8ff9f44c2af6f38ee78e821d6b4d84c9cde24b4
foster154 <foster154@gmail.com>
2015-01-08 06:23:29 -0700

    Fix guest#index and make results page responsive

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,7 +2,7 @@ class Guest < ActiveRecord::Base
 	has_many :queries
 	accepts_nested_attributes_for :queries
 
-	validates :email, presence: true, on: :update
+	validates_presence_of :email, on: :update
 
 	self.per_page = 20
 end
