aee210c7ef7d2396a89359daf435577e783027c8
foster154 <foster154@gmail.com>
2015-01-24 17:28:23 -0700

    add guest mailer, email after info request

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 
   config.action_mailer.delivery_method = :smtp
@@ -98,5 +99,4 @@ Rails.application.configure do
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
-}
 end
