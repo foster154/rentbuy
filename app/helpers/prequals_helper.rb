eda31a64eba7d2004c24ed216dc4843f466bb583
foster154 <foster154@gmail.com>
2015-02-24 07:28:56 -0700

    Improve prequal equation

--- app/helpers/prequals_helper.rb
+++ app/helpers/prequals_helper.rb
@@ -7,10 +7,34 @@ module PrequalsHelper
 		r = 0.06
 		n = 360
 
-		$can_afford_income = (m * ((1 + r/12)**n - 1)) / ((r/12) * (1 + r/12)**n)
-		
-		$can_afford_down = down_payment * 20
+		$max_principle = (m * ((1 + r/12)**n - 1)) / ((r/12) * (1 + r/12)**n)
 
+		$check_down_payment = (down_payment - $max_principle / 0.965 * 0.035)
+
+		$can_afford_income = $max_principle / 0.965 + (down_payment - $max_principle / 0.965 * 0.035)
+		$can_afford_down = down_payment / 0.035
+
+		if $check_down_payment >= 0
+			$your_number = $can_afford_income
+		else
+			$your_number = $can_afford_down
+		end
 	end
-
 end
+
+# module PrequalsHelper
+
+# 	def preapproved(income, debt, down_payment)
+# 		$max_monthly_payment = ((income / 12) - debt) * 0.4
+
+# 		m = $max_monthly_payment
+# 		r = 0.06
+# 		n = 360
+
+# 		$can_afford_income = (m * ((1 + r/12)**n - 1)) / ((r/12) * (1 + r/12)**n)
+		
+# 		$can_afford_down = down_payment * 20
+
+# 	end
+
+# end
