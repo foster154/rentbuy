53fc2e3f09a7eb06d81bb0e59336e84179561e12
foster154 <foster154@gmail.com>
2015-05-07 21:02:15 -0600

    Fix email format for Followup Boss

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -4,7 +4,7 @@ class GuestMailer < ActionMailer::Base
   def to_guest(guest)
     @guest = guest
     if Rails.env.production?
-    	mail to: @guest.email, bcc: "gcrum@iccu.com, myrentwillbuy@gmail.com, foster154@gmail.com", reply_to: "gcrum@iccu.com", subject: "My Rent Will Buy: Congratuations!"
+    	mail to: @guest.email, bcc: "gcrum@iccu.com, myrentwillbuy@gmail.com", reply_to: "gcrum@iccu.com", subject: "My Rent Will Buy: Congratuations!"
     else
     	mail to: "foster154@gmail.com", bcc: "myrentwillbuy@gmail.com", reply_to: "gcrum@iccu.com", subject: "(DEV) My Rent Will Buy: Congratuations!"
 	end
