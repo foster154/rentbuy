930ec470f0ad42f2f3df0cf611eec8a636da6da0
foster154 <foster154@gmail.com>
2015-05-08 11:17:55 -0600

    take Gabe bcc off of email to guest

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -4,7 +4,7 @@ class GuestMailer < ActionMailer::Base
   def to_guest(guest)
     @guest = guest
     if Rails.env.production?
-    	mail to: @guest.email, bcc: "gcrum@iccu.com, myrentwillbuy@gmail.com", reply_to: "gcrum@iccu.com", subject: "My Rent Will Buy: Congratuations!"
+    	mail to: @guest.email, bcc: "myrentwillbuy@gmail.com", reply_to: "gcrum@iccu.com", subject: "My Rent Will Buy: Congratuations!"
     else
     	mail to: "foster154@gmail.com", bcc: "myrentwillbuy@gmail.com", reply_to: "gcrum@iccu.com", subject: "(DEV) My Rent Will Buy: Congratuations!"
 	end
@@ -22,7 +22,7 @@ class GuestMailer < ActionMailer::Base
   def to_agent_update(guest)
     @guest = guest
     if Rails.env.production?
-    	mail to: "team.neal3@followupboss.me", bcc: "myrentwillbuy@gmail.com, foster154@gmail.com", subject: "Updated Lead from My Rent Will Buy"
+    	mail to: "team.neal3@followupboss.me", bcc: "myrentwillbuy@gmail.com", subject: "Updated Lead from My Rent Will Buy"
     else
     	mail to: "foster154@gmail.com", cc: "myrentwillbuy@gmail.com", subject: "(DEV) Updated Lead from My Rent Will Buy"
 	end
